bf6130622af77818dbea4b35b6f33028faeead7f
WinAlreadyLor <sandyongsowt@gmail.com>
2018-05-22 15:07:57 +0800

    added method in Layout master to update cart qty

--- SA46Team12BookShopApp/Layout.Master.cs
+++ SA46Team12BookShopApp/Layout.Master.cs
@@ -9,9 +9,16 @@ namespace SA46Team12BookShopApp
 {
     public partial class Layout : System.Web.UI.MasterPage
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        public void ChangeCartItemQty(string newQty)
+        {
+            Label lbl = (Label)this.FindControl("CartItemQty");
+            lbl.Text = newQty;
         }
     }
 }
