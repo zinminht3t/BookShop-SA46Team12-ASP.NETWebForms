c6dda095d6957d5b32eb61184b069381184e50f9
Jayden <wwj.jayden@gmail.com>
2018-05-22 10:26:07 +0800

    Query Admin Page

--- SA46Team12BookShopApp/Owner/Default.aspx.cs
+++ SA46Team12BookShopApp/Owner/Default.aspx.cs
@@ -5,79 +5,152 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace SA46Team12BookShopApp.Owner
 {
     public partial class Default : System.Web.UI.Page
     {
-        private SqlConnection conn;
-        
+        private string connection;
+        private string sqlquery;
+        private DataTable dtbl;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            connection = @"Data Source=localhost; Initial Catalog=Bookshop; Integrated Security=SSPI;";
             if (!IsPostBack)
             {
-                gvbind();
+                populate();
             }
         }
 
         protected void gbEditBooks_RowEditing(object sender, GridViewEditEventArgs e)
         {
             gvEditBooks.EditIndex = e.NewEditIndex;
-            gvbind();
+            populate();
         }
+
         protected void gbEditBooks_RowUpdating(object sender, GridViewUpdateEventArgs e)
         {
-            //int userid = Convert.ToInt32(gvEditBooks.DataKeys[e.RowIndex].Value.ToString());
-            //GridViewRow row = (GridViewRow)gvEditBooks.Rows[e.RowIndex];
-            //Label bookID = (Label)row.FindControl("BookID");
-            ////TextBox txtname=(TextBox)gr.cell[].control[];  
-            //TextBox textName = (TextBox)row.Cells[0].Controls[0];
-            //TextBox textadd = (TextBox)row.Cells[1].Controls[0];
-            //TextBox textc = (TextBox)row.Cells[2].Controls[0];
-            ////TextBox textadd = (TextBox)row.FindControl("txtadd");  
-            ////TextBox textc = (TextBox)row.FindControl("txtc");  
-            //gvEditBooks.EditIndex = -1;
-            //conn.Open();
-            ////SqlCommand cmd = new SqlCommand("SELECT * FROM detail", conn);  
-            //SqlCommand cmd = new SqlCommand("update BooksDB set name='" + textName.Text + "',address='" + textadd.Text + "',country='" + textc.Text + "'where id='" + userid + "'", conn);
-            //cmd.ExecuteNonQuery();
-            //conn.Close();
-            //gvbind();
-            ////GridView1.DataBind();  
-            
+            try
+            {
+                UpdateDatabase(e);
+            }
+            catch (Exception ex)
+            {
+                lblError.Visible = true;
+                lblError.Text = ex.Message;
+            }
         }
+
         protected void gbEditBooks_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
             gvEditBooks.PageIndex = e.NewPageIndex;
-            gvbind();
+            populate();
         }
         protected void gbEditBooks_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
         {
             gvEditBooks.EditIndex = -1;
-            gvbind();
+            populate();
         }
 
-        protected void SQLCon()
+        protected void populate()
         {
-            conn = new SqlConnection("Data Source=BooksDB;Integrated Security=true;Initial Catalog=SSPI");
+            sqlquery = "SELECT Book.BookID, Book.Title, Category.Name, Book.Author, " +
+                "Book.ISBN, Book.Stock, Book.Price, ISNULL(Discount.DiscountPercent, '1') AS DiscountPercent, CAST(ROUND(Book.Price*ISNULL(Discount.DiscountPercent,1), 2) AS numeric(10,2)) AS TOTAL " +
+                "FROM Book INNER JOIN Category ON Book.CategoryID = Category.CategoryID " +
+                "LEFT OUTER JOIN Discount ON Book.BookID = Discount.BookID";
+
+            SearchDatabase(sqlquery);
         }
 
-        protected void gvbind()
+        protected void tbSearch_TextChanged(object sender, EventArgs e)
         {
-            using (BooksDB b = new BooksDB())
+            sqlquery = "SELECT Book.BookID, Book.Title, Category.Name, Book.Author, " +
+                "Book.ISBN, Book.Stock, Book.Price, ISNULL(Discount.DiscountPercent, '1') AS DiscountPercent, CAST(ROUND(Book.Price*ISNULL(Discount.DiscountPercent,1), 2) AS numeric(10,2)) AS TOTAL " +
+                "FROM Book INNER JOIN Category ON Book.CategoryID = Category.CategoryID " +
+                "LEFT OUTER JOIN Discount ON Book.BookID = Discount.BookID " +
+                "WHERE Book.Title LIKE '%" + tbSearch.Text + "%'";
+
+            SearchDatabase(sqlquery);
+        }
+
+        protected void NoRecordsFound()
+        {
+            dtbl.Rows.Add(dtbl.NewRow());
+            gvEditBooks.DataSource = dtbl;
+            gvEditBooks.DataBind();
+            gvEditBooks.Rows[0].Cells.Clear();
+            gvEditBooks.Rows[0].Cells.Add(new TableCell());
+            gvEditBooks.Rows[0].Cells[0].ColumnSpan = dtbl.Columns.Count;
+            gvEditBooks.Rows[0].Cells[0].Text = "No Records Found";
+            gvEditBooks.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
+        }
+
+        protected void SearchDatabase(string sqlquery)
+        {
+            dtbl = new DataTable();
+            using (SqlConnection sqlcon = new SqlConnection(connection))
             {
-                if (b.Books.ToList<Book>().Count() > 0)
-                {
-                    gvEditBooks.DataSource = b.Books.ToList<Book>();
-                    gvEditBooks.DataBind();
-                }
-                else
-                {
-                    Response.Write("No Records Found");
-                }
+                sqlcon.Open();
+                SqlDataAdapter sqlda = new SqlDataAdapter(sqlquery, sqlcon);
+                sqlda.Fill(dtbl);
+                sqlcon.Close();
+            }
+            if (dtbl.Rows.Count > 0)
+            {
+                gvEditBooks.DataSource = dtbl;
+                gvEditBooks.DataBind();
+            }
+            else
+            {
+                NoRecordsFound();
             }
         }
+
+        protected void UpdateDatabase(GridViewUpdateEventArgs e)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(connection))
+            {
+                sqlcon.Open();
+                string sql = "  Update book set Stock = @qty , price = @price where bookid = @id";
+                SqlCommand sqlcom = new SqlCommand(sql, sqlcon);
+                sqlcom.Parameters.AddWithValue("qty", (gvEditBooks.Rows[e.RowIndex].FindControl("tbQty") as TextBox).Text.Trim());
+                sqlcom.Parameters.AddWithValue("price", (gvEditBooks.Rows[e.RowIndex].FindControl("tbPrice") as TextBox).Text.Trim());
+                sqlcom.Parameters.AddWithValue("id", (gvEditBooks.Rows[e.RowIndex].FindControl("lblBookID") as Label).Text);
+                sqlcom.ExecuteNonQuery();
+
+                sql = "Update Discount set DiscountPercent=@disc where bookid =@id";
+                sqlcom = new SqlCommand(sql, sqlcon);
+                sqlcom.Parameters.AddWithValue("disc", (gvEditBooks.Rows[e.RowIndex].FindControl("tbDiscP") as TextBox).Text.Trim());
+                sqlcom.Parameters.AddWithValue("id", (gvEditBooks.Rows[e.RowIndex].FindControl("lblBookID") as Label).Text);
+                sqlcom.ExecuteNonQuery();
+
+                gvEditBooks.EditIndex = -1;
+                populate();
+                lblSuccess.Visible = true;
+            }
+        }
+
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            tbSearch.Text = String.Empty;
+        }
+
+        protected void btnViewAll_Click(object sender, EventArgs e)
+        {
+            populate();
+        }
+
+        protected void ddlCategoryFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sqlquery = "SELECT Book.BookID, Book.Title, Category.Name, Book.Author, " +
+                "Book.ISBN, Book.Stock, Book.Price, ISNULL(Discount.DiscountPercent, '1') AS DiscountPercent, CAST(ROUND(Book.Price*ISNULL(Discount.DiscountPercent,1), 2) AS numeric(10,2)) AS TOTAL " +
+                "FROM Book INNER JOIN Category ON Book.CategoryID = Category.CategoryID " +
+                "LEFT OUTER JOIN Discount ON Book.BookID = Discount.BookID " +
+                "WHERE Category.Name = '" + ddlCategoryFilter.SelectedItem.Text + "'";
+
+            SearchDatabase(sqlquery);
+        }
     }
-
-
 }
--- SA46Team12BookShopApp/Owner/Default.aspx.designer.cs
+++ SA46Team12BookShopApp/Owner/Default.aspx.designer.cs
@@ -22,6 +22,69 @@ namespace SA46Team12BookShopApp.Owner {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// lblTitle control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTitle;
+        
+        /// <summary>
+        /// Panel1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel1;
+        
+        /// <summary>
+        /// ddlCategoryFilter control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlCategoryFilter;
+        
+        /// <summary>
+        /// lblSearch control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblSearch;
+        
+        /// <summary>
+        /// tbSearch control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbSearch;
+        
+        /// <summary>
+        /// btnReset control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnReset;
+        
+        /// <summary>
+        /// btnViewAll control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnViewAll;
+        
+        /// <summary>
         /// gvEditBooks control.
         /// </summary>
         /// <remarks>
@@ -29,5 +92,32 @@ namespace SA46Team12BookShopApp.Owner {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gvEditBooks;
+        
+        /// <summary>
+        /// lblSuccess control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblSuccess;
+        
+        /// <summary>
+        /// lblError control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblError;
+        
+        /// <summary>
+        /// SqlDataSource2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource2;
     }
 }
