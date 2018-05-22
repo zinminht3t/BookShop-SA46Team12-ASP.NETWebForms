097d1f38a1000beb24550460603523f0246f5f11
Jayden <wwj.jayden@gmail.com>
2018-05-22 22:43:20 +0800

    admin final

--- SA46Team12BookShopApp/Owner/Default.aspx.cs
+++ SA46Team12BookShopApp/Owner/Default.aspx.cs
@@ -131,7 +131,7 @@ namespace SA46Team12BookShopApp.Owner
                     lblSuccess.Visible = true;
                 }
             }
-            catch (Exception ex)
+            catch (Exception)
             {
             }
         }
