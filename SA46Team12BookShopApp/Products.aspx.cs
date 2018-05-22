1e492966ae4a7c0665018ba71b71963747fba9c1
Zin Min Htet <zinminht3t@gmail.com>
2018-05-22 22:23:34 +0800

    after logout error add to cart fixed

--- SA46Team12BookShopApp/Products.aspx.cs
+++ SA46Team12BookShopApp/Products.aspx.cs
@@ -147,14 +147,16 @@ namespace SA46Team12BookShopApp
                     buttonclicked.Text = "Remove from Cart";
                     buttonclicked.CssClass = "btn btn-primary buttonClicked";
                     cartItems = (List<int>)Session["cart_items"];    // GET
-                    cartItems.Add(this.itemClicked);
-                    Master.ChangeCartItemQty(cartItems.Count.ToString());
-                    Session["cart_items"] = cartItems;
 
                     if (cartItems == null)
                     {
                         cartItems = new List<int>();
                     }
+
+                    cartItems.Add(this.itemClicked);
+                    Master.ChangeCartItemQty(cartItems.Count.ToString());
+                    Session["cart_items"] = cartItems;
+
                     
                 }
                 else
